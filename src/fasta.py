8000cfd09c9cf07f249f38eb40558fcf802e0a18
Hao Wang <hao.wang@chalmers.se>
2021-08-09 20:33:42 +0200

    style: add docstrings

--- src/fasta.py
+++ src/fasta.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
+import requests as r
 from Bio import SeqIO
+from io import StringIO
+
+
+def get_uniprot_seq(uniprot_id):
+    '''
+    Download UniProt protein sequence and retrun as a SeqRecord object
+    '''
+    uniprotUrl = "http://www.uniprot.org/uniprot/"
+    response = r.post(uniprotUrl+uniprot_id+".fasta")
+    # TO DO: check response
+    Seq = StringIO(''.join(response.text))
+    return list(SeqIO.parse(Seq, 'fasta'))
 
 
 def sort_fasta_file(ref_fasta, fasta_to_sort):
-    """
-    
-    """
+    '''
+    Sort an input fasta in SeqRecord object based on a reference fasta
+    :param ref_fasta: a reference list of fasta
+    :param fasta_to_sort: a list of fasta to be sorted
+    '''
     sorted_fasta = []
 
     for elem in ref_fasta:
--- src/parsers.py
+++ src/parsers.py
@@ -6,6 +6,9 @@ import pandas as pd
 def get_column_from_tsv(tsv_file, column_id, to_list=True):
     """
     read a column from a tsv file and convert the content into a list by default
+    :param tsv_file: a tsv file to read
+    :param column_id: 
+    :param to_list: 
     """
     tsv_content = pd.read_table(tsv_file)
     target_column = tsv_content[column_id]
@@ -17,9 +20,14 @@ def get_column_from_tsv(tsv_file, column_id, to_list=True):
 def get_column_from_csv(csv_file, column_id, to_list=True):
     """
     read a column from a csv file and convert the content into a list by default
+    :param csv_file: a csv file to read
+    :param column_id: 
+    :param to_list: 
     """
     csv_content = pd.read_csv(csv_file)
     target_column = csv_content[column_id]
     if to_list:
         return target_column.to_list()
     return target_column
+
+
