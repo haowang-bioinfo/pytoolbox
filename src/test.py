e6a1f83c5ce49b53bf9e74d4c67e8963c54634d2
Hao-Chalmers <hao.wang@chalmers.se>
2021-08-10 19:13:26 +0200

    feat: add test

--- src/test.py
+++ src/test.py
@@ -1,6 +1,15 @@
 
+from numpy import record
 import mapid
+import fasta
+from Bio import SeqIO
+from io import StringIO
 
+# test for filter_fasta_seq
+seq = list(SeqIO.parse(StringIO('>abc\nAGCTNACTG'), 'fasta'))
+assert fasta.filter_fasta_seqs(seq) == [], "failed to filter sequences."
+
+# test for get_ccds_id
 uniprotid = "P14550"
 ccdsid = "CCDS523.1"
 assert ccdsid == mapid.get_ccds_id(uniprotid), "failed in mapping CCDS id."
